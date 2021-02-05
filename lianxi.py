0aab024c6308f38f5e4ab28d2365170382e9eff7
PC-398 <771293871@qq.cpm>
2021-02-05 11:27:50 +0800

    首次上传

--- lianxi.py
+++ lianxi.py
@@ -4,27 +4,43 @@ from requests_toolbelt import MultipartEncoder
 import csv
 import time
 import os
-import os
-for (root,dirs,files) in os.walk("E:\old"):
-    i=1
-    j=1
+import csv
+for (root,dirs,files) in os.walk("D:/接口测试/jemter/save/PPG项目/ECG数据未标定/mimicecg1"):
     for f in files:
         old_path = os.path.join(root,f)
-        # 把文件名分解为 文件名.扩展名
-        # 在这里可以添加一个 filter，过滤掉不想复制的文件类型，或者文件名
-        (shotname, extension) = os.path.splitext(f)
-        # 原文件的路径
-        if 'old' in shotname:
-            new_name = "窦性"+str(i)+ extension
-            new_path = os.path.join("E:/new1", new_name)
-            # 新文件的路径
-            i = i + 1
-        elif 'new' in shotname:
-            new_name = "房颤"+str(j)+ extension
-            new_path = os.path.join("E:/new2", new_name)
-            # 新文件的路径
-            j = j + 1
-        open(new_path,'wb').write(open(old_path,'rb').read())
+        print(old_path)
+        with open('D:/接口测试/jemter/save/PPG项目/ECG数据未标定/112.csv','a',newline='')as f1:
+            spam = csv.writer(f1)
+            spam.writerow([old_path])
+
+
+
+
+
+
+
+
+
+# for (root,dirs,files) in os.walk("E:\old"):
+#     i=1
+#     j=1
+#     for f in files:
+#         old_path = os.path.join(root,f)
+#         # 把文件名分解为 文件名.扩展名
+#         # 在这里可以添加一个 filter，过滤掉不想复制的文件类型，或者文件名
+#         (shotname, extension) = os.path.splitext(f)
+#         # 原文件的路径
+#         if 'old' in shotname:
+#             new_name = "窦性"+str(i)+ extension
+#             new_path = os.path.join("E:/new1", new_name)
+#             # 新文件的路径
+#             i = i + 1
+#         elif 'new' in shotname:
+#             new_name = "房颤"+str(j)+ extension
+#             new_path = os.path.join("E:/new2", new_name)
+#             # 新文件的路径
+#             j = j + 1
+#         open(new_path,'wb').write(open(old_path,'rb').read())
 
 
 
